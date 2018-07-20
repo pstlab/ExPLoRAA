cc5192da4b6d7836e22d9a760bcea8d5a5df479b
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-07-20 13:00:43 +0200

    added initial mobile app..

---
+++ ExPLoRAA-MobileApp/app/src/main/java/it/cnr/istc/exploraa/NavigatorActivity.java
@@ -0,0 +1,12 @@
+package it.cnr.istc.exploraa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NavigatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
