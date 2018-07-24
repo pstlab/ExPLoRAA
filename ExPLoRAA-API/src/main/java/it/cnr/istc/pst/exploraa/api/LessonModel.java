a2215cf0fe6258673539ed129a6b0735d481840b
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-07-24 15:34:55 +0200

    improved adapters..

--- ExPLoRAA-API/src/main/java/it/cnr/istc/pst/exploraa/api/LessonModel.java
+++ ExPLoRAA-API/src/main/java/it/cnr/istc/pst/exploraa/api/LessonModel.java
@@ -19,9 +19,20 @@ package it.cnr.istc.pst.exploraa.api;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonString;
+import javax.json.JsonValue;
+import javax.json.bind.adapter.JsonbAdapter;
 
 /**
  *
@@ -29,14 +40,27 @@ import java.util.concurrent.TimeUnit;
  */
 public class LessonModel {
 
+    public static final LessonModelAdapter ADAPTER = new LessonModelAdapter();
     public Long id;
     public String name;
     public Map<String, StimulusTemplate> stimuli;
     public Set<String> ids;
     public Collection<Relation> relations;
 
+    public LessonModel() {
+    }
+
+    public LessonModel(Long id, String name, Map<String, StimulusTemplate> stimuli, Set<String> ids, Collection<Relation> relations) {
+        this.id = id;
+        this.name = name;
+        this.stimuli = stimuli;
+        this.ids = ids;
+        this.relations = relations;
+    }
+
     public static class StimulusTemplate {
 
+        public static final StimulusTemplateAdapter ADAPTER = new StimulusTemplateAdapter();
         public StimulusTemplateType type;
         public String name;
         public Set<String> topics;
@@ -122,6 +146,7 @@ public class LessonModel {
 
     public static class Condition {
 
+        public static final ConditionAdapter ADAPTER = new ConditionAdapter();
         public ConditionType type;
 
         public Condition() {
@@ -221,6 +246,7 @@ public class LessonModel {
 
     public static class Relation {
 
+        public static final RelationAdapter ADAPTER = new RelationAdapter();
         public String from;
         public String to;
         public Long lb;
@@ -236,6 +262,232 @@ public class LessonModel {
             this.lb = lb;
             this.ub = ub;
             this.unit = unit;
+        }
+    }
+
+    public static class LessonModelAdapter implements JsonbAdapter<LessonModel, JsonObject> {
+
+        @Override
+        public JsonObject adaptToJson(LessonModel obj) throws Exception {
+            JsonObjectBuilder lesson_model_builder = Json.createObjectBuilder();
+            lesson_model_builder.add("id", obj.id);
+            lesson_model_builder.add("name", obj.name);
+            JsonArrayBuilder stimuli_builder = Json.createArrayBuilder();
+            for (StimulusTemplate stimulus_template : obj.stimuli.values()) {
+                stimuli_builder.add(StimulusTemplate.ADAPTER.adaptToJson(stimulus_template));
+            }
+            lesson_model_builder.add("stimuli", stimuli_builder);
+            JsonArrayBuilder ids_builder = Json.createArrayBuilder();
+            for (String id : obj.ids) {
+                ids_builder.add(id);
+            }
+            lesson_model_builder.add("ids", ids_builder);
+            JsonArrayBuilder rels_builder = Json.createArrayBuilder();
+            for (Relation rel : obj.relations) {
+                rels_builder.add(Relation.ADAPTER.adaptToJson(rel));
+            }
+            lesson_model_builder.add("relations", rels_builder);
+            return lesson_model_builder.build();
+        }
+
+        @Override
+        public LessonModel adaptFromJson(JsonObject obj) throws Exception {
+            Map<String, StimulusTemplate> stimuli = new HashMap<>(obj.getJsonArray("stimuli").size());
+            for (JsonValue stimulus : obj.getJsonArray("stimuli")) {
+                StimulusTemplate template = StimulusTemplate.ADAPTER.adaptFromJson(stimulus.asJsonObject());
+                stimuli.put(template.name, template);
+            }
+            Set<String> ids = new HashSet<>(obj.getJsonArray("ids").size());
+            for (JsonValue id : obj.getJsonArray("ids")) {
+                ids.add(((JsonString) id).getString());
+            }
+            List<Relation> relations = new ArrayList<>(obj.getJsonArray("relations").size());
+            for (JsonValue rel_val : obj.getJsonArray("relations")) {
+                relations.add(Relation.ADAPTER.adaptFromJson(rel_val.asJsonObject()));
+            }
+            return new LessonModel(obj.containsKey("id") ? new Long(obj.getInt("id")) : null, obj.getString("name"), stimuli, ids, relations);
+        }
+    }
+
+    public static class ConditionAdapter implements JsonbAdapter<Condition, JsonObject> {
+
+        @Override
+        public JsonObject adaptToJson(Condition obj) throws Exception {
+            JsonObjectBuilder c_object = Json.createObjectBuilder();
+            c_object.add("type", obj.type.name());
+            switch (obj.type) {
+                case And:
+                    JsonArrayBuilder and_condition_builder = Json.createArrayBuilder();
+                    for (Condition condition : ((Condition.AndCondition) obj).conditions) {
+                        and_condition_builder.add(Condition.ADAPTER.adaptToJson(condition));
+                    }
+                    c_object.add("conditions", and_condition_builder);
+                    break;
+                case Or:
+                    JsonArrayBuilder or_condition_builder = Json.createArrayBuilder();
+                    for (Condition condition : ((Condition.OrCondition) obj).conditions) {
+                        or_condition_builder.add(Condition.ADAPTER.adaptToJson(condition));
+                    }
+                    c_object.add("conditions", or_condition_builder);
+                    break;
+                case Not:
+                    c_object.add("condition", Condition.ADAPTER.adaptToJson(((Condition.NotCondition) obj).condition));
+                    break;
+                case Numeric:
+                    c_object.add("numeric_condition_type", ((Condition.NumericCondition) obj).numeric_condition_type.name());
+                    c_object.add("variable", ((Condition.NumericCondition) obj).variable);
+                    c_object.add("value", ((Condition.NumericCondition) obj).value);
+                    break;
+                case Nominal:
+                    c_object.add("variable", ((Condition.NominalCondition) obj).variable);
+                    c_object.add("value", ((Condition.NominalCondition) obj).value);
+                    break;
+                default:
+                    throw new AssertionError(obj.type.name());
+            }
+            return c_object.build();
+        }
+
+        @Override
+        public Condition adaptFromJson(JsonObject obj) throws Exception {
+            switch (Condition.ConditionType.valueOf(obj.getString("type"))) {
+                case And:
+                    JsonArray and_conditions_array = obj.getJsonArray("conditions");
+                    Collection<Condition> and_conditions = new ArrayList<>(and_conditions_array.size());
+                    for (JsonValue cond_value : and_conditions_array) {
+                        and_conditions.add(Condition.ADAPTER.adaptFromJson(cond_value.asJsonObject()));
+                    }
+                    return new Condition.AndCondition(and_conditions);
+                case Or:
+                    JsonArray or_conditions_array = obj.getJsonArray("conditions");
+                    Collection<Condition> or_conditions = new ArrayList<>(or_conditions_array.size());
+                    for (JsonValue cond_value : or_conditions_array) {
+                        or_conditions.add(Condition.ADAPTER.adaptFromJson(cond_value.asJsonObject()));
+                    }
+                    return new Condition.OrCondition(or_conditions);
+                case Not:
+                    return new Condition.NotCondition(Condition.ADAPTER.adaptFromJson(obj.getJsonObject("condition")));
+                case Numeric:
+                    return new Condition.NumericCondition(Condition.NumericCondition.NumericConditionType.valueOf(obj.getString("numeric_condition_type")), obj.getString("variable"), obj.getJsonNumber("value").doubleValue());
+                case Nominal:
+                    return new Condition.NominalCondition(obj.getString("variable"), obj.getString("value"));
+                default:
+                    throw new AssertionError(Condition.ConditionType.valueOf(obj.getString("type")).name());
+            }
+        }
+    }
+
+    public static class StimulusTemplateAdapter implements JsonbAdapter<StimulusTemplate, JsonObject> {
+
+        @Override
+        public JsonObject adaptToJson(StimulusTemplate obj) throws Exception {
+            JsonObjectBuilder stimulus_builder = Json.createObjectBuilder();
+            stimulus_builder.add("type", obj.type.name());
+            stimulus_builder.add("name", obj.name);
+            JsonArrayBuilder topics_builder = Json.createArrayBuilder();
+            for (String topic : obj.topics) {
+                topics_builder.add(topic);
+            }
+            stimulus_builder.add("topics", topics_builder);
+            if (obj.trigger_condition != null) {
+                stimulus_builder.add("trigger_condition", Condition.ADAPTER.adaptToJson(obj.trigger_condition));
+            }
+            if (obj.execution_condition != null) {
+                stimulus_builder.add("execution_condition", Condition.ADAPTER.adaptToJson(obj.execution_condition));
+            }
+            JsonArrayBuilder ids_builder = Json.createArrayBuilder();
+            for (String id : obj.ids) {
+                ids_builder.add(id);
+            }
+            stimulus_builder.add("ids", ids_builder);
+            JsonArrayBuilder rels_builder = Json.createArrayBuilder();
+            for (Relation rel : obj.relations) {
+                rels_builder.add(Relation.ADAPTER.adaptToJson(rel));
+            }
+            stimulus_builder.add("relations", rels_builder);
+            switch (obj.type) {
+                case Root:
+                    break;
+                case Text:
+                    stimulus_builder.add("content", ((StimulusTemplate.TextStimulusTemplate) obj).content);
+                    break;
+                case URL:
+                    stimulus_builder.add("content", ((StimulusTemplate.URLStimulusTemplate) obj).content);
+                    stimulus_builder.add("url", ((StimulusTemplate.URLStimulusTemplate) obj).url);
+                    break;
+                case Question:
+                    stimulus_builder.add("question", ((StimulusTemplate.QuestionStimulusTemplate) obj).question);
+                    JsonArrayBuilder ans_builder = Json.createArrayBuilder();
+                    for (StimulusTemplate.QuestionStimulusTemplate.Answer answer : ((StimulusTemplate.QuestionStimulusTemplate) obj).answers) {
+                        ans_builder.add(Json.createObjectBuilder().add("answer", answer.answer).add("event", answer.event));
+                    }
+                    stimulus_builder.add("answers", ans_builder);
+                    break;
+                default:
+                    throw new AssertionError(obj.type.name());
+            }
+            return stimulus_builder.build();
+        }
+
+        @Override
+        public StimulusTemplate adaptFromJson(JsonObject obj) throws Exception {
+            String name = obj.getString("name");
+            Set<String> topics = new HashSet<>(obj.getJsonArray("topics").size());
+            JsonArray topics_array = obj.getJsonArray("topics");
+            for (JsonValue topic : topics_array) {
+                topics.add(((JsonString) topic).getString());
+            }
+            Condition trigger_condition = obj.containsKey("trigger_condition") && !obj.isNull("trigger_condition") ? Condition.ADAPTER.adaptFromJson(obj.getJsonObject("trigger_condition")) : null;
+            Condition execution_condition = obj.containsKey("execution_condition") && !obj.isNull("execution_condition") ? Condition.ADAPTER.adaptFromJson(obj.getJsonObject("execution_condition")) : null;
+            Set<String> ids = new HashSet<>(obj.getJsonArray("ids").size());
+            for (JsonValue id : obj.getJsonArray("ids")) {
+                ids.add(((JsonString) id).getString());
+            }
+            List<Relation> relations = new ArrayList<>(obj.getJsonArray("relations").size());
+            for (JsonValue rel_val : obj.getJsonArray("relations")) {
+                relations.add(Relation.ADAPTER.adaptFromJson(rel_val.asJsonObject()));
+            }
+            switch (StimulusTemplate.StimulusTemplateType.valueOf(obj.getString("type"))) {
+                case Root:
+                    return new StimulusTemplate(StimulusTemplate.StimulusTemplateType.Root, name, topics, trigger_condition, execution_condition, ids, relations);
+                case Text:
+                    return new StimulusTemplate.TextStimulusTemplate(name, topics, trigger_condition, execution_condition, ids, relations, obj.getString("content"));
+                case URL:
+                    return new StimulusTemplate.URLStimulusTemplate(name, topics, trigger_condition, execution_condition, ids, relations, obj.getString("content"), obj.getString("url"));
+                case Question:
+                    JsonArray answers_array = obj.getJsonArray("answers");
+                    List<StimulusTemplate.QuestionStimulusTemplate.Answer> answers = new ArrayList<>(answers_array.size());
+                    for (JsonValue ans_value : answers_array) {
+                        JsonObject ans_onject = ans_value.asJsonObject();
+                        answers.add(new StimulusTemplate.QuestionStimulusTemplate.Answer(ans_onject.getString("answer"), ans_onject.getString("event")));
+                    }
+                    return new StimulusTemplate.QuestionStimulusTemplate(name, topics, trigger_condition, execution_condition, ids, relations, obj.getString("question"), answers);
+                default:
+                    throw new AssertionError(StimulusTemplate.StimulusTemplateType.valueOf(obj.getString("type")).name());
+            }
+        }
+    }
+
+    public static class RelationAdapter implements JsonbAdapter<Relation, JsonObject> {
+
+        @Override
+        public JsonObject adaptToJson(Relation obj) throws Exception {
+            JsonObjectBuilder rel_builder = Json.createObjectBuilder();
+            rel_builder.add("from", obj.from);
+            rel_builder.add("to", obj.to);
+            if (obj.lb != Double.NEGATIVE_INFINITY) {
+                rel_builder.add("lb", obj.lb);
+            }
+            if (obj.ub != Double.POSITIVE_INFINITY) {
+                rel_builder.add("ub", obj.ub);
+            }
+            rel_builder.add("unit", obj.unit.name());
+            return rel_builder.build();
+        }
+
+        @Override
+        public Relation adaptFromJson(JsonObject obj) throws Exception {
+            return new Relation(obj.getString("from"), obj.getString("to"), obj.containsKey("lb") ? obj.getJsonNumber("lb").longValue() : null, obj.containsKey("ub") ? obj.getJsonNumber("ub").longValue() : null, TimeUnit.valueOf(obj.getString("unit")));
         }
     }
 }
--- ExPLoRAA-API/src/main/java/it/cnr/istc/pst/exploraa/api/User.java
+++ ExPLoRAA-API/src/main/java/it/cnr/istc/pst/exploraa/api/User.java
@@ -16,7 +16,17 @@
  */
 package it.cnr.istc.pst.exploraa.api;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonString;
+import javax.json.JsonValue;
+import javax.json.bind.adapter.JsonbAdapter;
 
 /**
  *
@@ -24,6 +34,7 @@ import java.util.Map;
  */
 public class User {
 
+    public static final UserAdapter ADAPTER = new UserAdapter();
     public long id;
     public String email;
     public String first_name;
@@ -47,5 +58,95 @@ public class User {
         this.par_values = par_values;
         this.follows = follows;
         this.teachs = teachs;
+    }
+
+    public static class UserAdapter implements JsonbAdapter<User, JsonObject> {
+
+        @Override
+        public JsonObject adaptToJson(User obj) throws Exception {
+            JsonObjectBuilder user_builder = Json.createObjectBuilder();
+            user_builder.add("id", obj.id);
+            user_builder.add("email", obj.email);
+            user_builder.add("first_name", obj.first_name);
+            user_builder.add("last_name", obj.last_name);
+            user_builder.add("online", obj.online);
+            JsonArrayBuilder par_types_builder = Json.createArrayBuilder();
+            for (Parameter par_type : obj.par_types.values()) {
+                par_types_builder.add(Parameter.ADAPTER.adaptToJson(par_type));
+            }
+            user_builder.add("par_types", par_types_builder);
+            JsonObjectBuilder par_values_builder = Json.createObjectBuilder();
+            for (Map.Entry<String, Map<String, String>> entry : obj.par_values.entrySet()) {
+                JsonObjectBuilder par_val_builder = Json.createObjectBuilder();
+                for (Map.Entry<String, String> sub_par_val : entry.getValue().entrySet()) {
+                    par_val_builder.add(sub_par_val.getKey(), sub_par_val.getValue());
+                }
+            }
+            user_builder.add("par_values", par_values_builder);
+            JsonArrayBuilder follows_builder = Json.createArrayBuilder();
+            for (Follow follow : obj.follows.values()) {
+                JsonObjectBuilder follow_builder = Json.createObjectBuilder();
+                follow_builder.add("lesson", Lesson.ADAPTER.adaptToJson(follow.lesson));
+                JsonArrayBuilder interests_builder = Json.createArrayBuilder();
+                for (String interest : follow.interests) {
+                    interests_builder.add(interest);
+                }
+                user_builder.add("interests", interests_builder);
+                follows_builder.add(follow_builder);
+            }
+            user_builder.add("follows", follows_builder);
+            JsonArrayBuilder teachs_builder = Json.createArrayBuilder();
+            for (Teach teach : obj.teachs.values()) {
+                JsonObjectBuilder teach_builder = Json.createObjectBuilder();
+                teach_builder.add("lesson", Lesson.ADAPTER.adaptToJson(teach.lesson));
+                teachs_builder.add(teach_builder);
+            }
+            user_builder.add("follows", teachs_builder);
+            return user_builder.build();
+        }
+
+        @Override
+        public User adaptFromJson(JsonObject obj) throws Exception {
+            int id = obj.getInt("email");
+            String email = obj.getString("email");
+            String first_name = obj.getString("first_name");
+            String last_name = obj.getString("last_name");
+            boolean online = obj.getBoolean("online");
+
+            Map<String, Parameter> par_types = new HashMap<>(obj.getJsonArray("par_types").size());
+            for (JsonValue par_type : obj.getJsonArray("par_types")) {
+                Parameter p = Parameter.ADAPTER.adaptFromJson(par_type.asJsonObject());
+                par_types.put(p.name, p);
+            }
+
+            Map<String, Map<String, String>> par_values = new HashMap<>();
+            for (Map.Entry<String, JsonValue> entry : obj.getJsonObject("par_values").entrySet()) {
+                Map<String, String> par_val = new HashMap<>();
+                for (Map.Entry<String, JsonValue> sub_par_val : entry.getValue().asJsonObject().entrySet()) {
+                    par_val.put(sub_par_val.getKey(), ((JsonString) sub_par_val.getValue()).getString());
+                }
+                par_values.put(entry.getKey(), par_val);
+            }
+
+            Map<Long, Follow> follows = new HashMap<>(obj.getJsonArray("follows").size());
+            for (JsonValue follow : obj.getJsonArray("follows")) {
+                JsonObject follow_object = follow.asJsonObject();
+                Lesson l = Lesson.ADAPTER.adaptFromJson(follow_object.getJsonObject("lesson"));
+                Set<String> interests = new HashSet<>(obj.getJsonArray("interests").size());
+                for (JsonValue interest : obj.getJsonArray("interests")) {
+                    interests.add(((JsonString) interest).getString());
+                }
+                follows.put(l.id, new Follow(null, l, interests));
+            }
+
+            Map<Long, Teach> teachs = new HashMap<>(obj.getJsonArray("teachs").size());
+            for (JsonValue teach : obj.getJsonArray("teachs")) {
+                JsonObject teach_object = teach.asJsonObject();
+                Lesson l = Lesson.ADAPTER.adaptFromJson(teach_object.getJsonObject("lesson"));
+                teachs.put(l.id, new Teach(null, l));
+            }
+
+            return new User(id, email, first_name, last_name, online, par_types, par_values, follows, teachs);
+        }
     }
 }
