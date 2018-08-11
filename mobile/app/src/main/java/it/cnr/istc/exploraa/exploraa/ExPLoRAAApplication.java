ae9fb33c5ab4e4158a457452b6b190a095222b89
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-08-11 12:22:32 +0200

    added initial mobile app..

---
+++ mobile/app/src/main/java/it/cnr/istc/exploraa/exploraa/ExPLoRAAApplication.java
@@ -0,0 +1,6 @@
+package it.cnr.istc.exploraa.exploraa;
+
+import android.app.Application;
+
+public class ExPLoRAAApplication extends Application {
+}
---
+++ mobile/app/src/main/java/it/cnr/istc/exploraa/exploraa/MainActivity.java
@@ -0,0 +1,13 @@
+package it.cnr.istc.exploraa.exploraa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
